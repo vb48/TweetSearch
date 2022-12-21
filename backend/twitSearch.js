b0effe87779e64f1d93febeb8bcb94556a489cb8
Vaibhav Bijapur <97873754+vb48@users.noreply.github.com>
2022-12-21 23:55:35 +0530

    Update twitSearch.js

--- backend/twitSearch.js
+++ backend/twitSearch.js
@@ -1,10 +1,10 @@
 const twit  = require('twit');
 
 const client = new twit({
-    consumer_key : 'mKihbWZ6RjhcsaZiuw1IOHHRF',
-    consumer_secret: 'vNVK49iWS3qr7N9QrhkbSP6FN58VHmEMd1U3s8zRwdXRADxwXu',
-    access_token : '1032979442157711360-c9fwEaM7yXdQPcgzhFqoDoVOHEW7D3',
-    access_token_secret : 'ArjorNeCPWH3bQ3KUx7MRGnMzjGPfCeCPeZztWMOWdItz'
+    consumer_key : 'ADD_YOUR_KEY_HERE',
+    consumer_secret: 'ADD_YOUR_KEY_HERE',
+    access_token : 'ADD_YOUR_KEY_HERE',
+    access_token_secret : 'ADD_YOUR_KEY_HERE'
 })
 
 module.exports = client
